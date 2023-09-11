264a558ad0ab39b2ee88bdb0d66efa7f776d7edb
hahahahahahayu <143996874+hahahahahahayu@users.noreply.github.com>
2023-09-11 15:48:23 +0800

    Add files via upload

---
+++ 排序.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <algorithm> 
+#include <numeric> 
+using namespace std;
+
+int main()
+{
+	int n; 
+	cin >> n;
+	int arr[10];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	std::sort(arr, arr + n, std::greater<int>());
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	int min_val = *min_element(arr, arr + n);
+	int max_val = *max_element(arr, arr + n);
+	double sum = accumulate(arr, arr + n, 0); 
+	double avg_val = sum / n;
+
+	cout << "最小值：" << min_val << endl;
+	cout << "最大值：" << max_val << endl;
+	cout << "平均值：" << avg_val << endl;
+	return 0;
+}
